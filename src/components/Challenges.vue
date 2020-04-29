f19c85273e2db41f0360f20c4f546eb85f39e660
Jesus Quinto <jesuskinto@gmail.com>
2020-04-29 14:43:39 -0400

    new Challenger

--- src/components/Challenges.vue
+++ src/components/Challenges.vue
@@ -3,73 +3,17 @@
     <h1 class="title">📚 {{ $t('challenges')}}</h1>
     <h2 class="subtitle">{{ $t('challenges-message')}}</h2>
     <div class="columns is-multiline">
-      <div class="column is-6">
+      <div v-for="r in results" :key="r" class="column is-6">
         <iframe data-aos="fade"
           scrolling="no"
           title="Clock"
-          src="https://codepen.io/jesuskinto/embed/preview/LYpYYXO?height=265&theme-id=light"
+          :src="`https://codepen.io/jesuskinto/embed/preview/${r}?height=265&theme-id=light`"
           frameborder="no"
           allowtransparency="true"
           allowfullscreen="true"
         >
           See the Pen
-          <a href="https://codepen.io/jesuskinto/pen/LYpYYXO">Clock</a> by Quinto Jesús
-          (
-          <a href="https://codepen.io/jesuskinto">@jesuskinto</a>) on
-          <a href="https://codepen.io">CodePen</a>.
-        </iframe>
-      </div>
-      <div class="column is-6">
-        <iframe
-          data-aos="fade"
-          scrolling="no"
-          title="Sliders"
-          src="https://codepen.io/jesuskinto/embed/preview/gOapmMj?height=265&theme-id=light"
-          frameborder="no"
-          allowtransparency="true"
-          allowfullscreen="true"
-          loading="lazy"
-        >
-          See the Pen
-          <a href="https://codepen.io/jesuskinto/pen/gOapmMj">Sliders</a> by Quinto Jesús
-          (
-          <a href="https://codepen.io/jesuskinto">@jesuskinto</a>) on
-          <a href="https://codepen.io">CodePen</a>.
-        </iframe>
-      </div>
-      <div class="column is-6">
-        <iframe
-          data-aos="fade"
-          scrolling="no"
-          title="Calculator"
-          src="https://codepen.io/jesuskinto/embed/preview/wvKGrwE?height=265&theme-id=light&default-tab=result"
-          frameborder="no"
-          allowtransparency="true"
-          allowfullscreen="true"
-          loading="lazy"
-        >
-          See the Pen
-          <a href="https://codepen.io/jesuskinto/pen/wvKGrwE">Calculator</a> by Quinto Jesús
-          (
-          <a href="https://codepen.io/jesuskinto">@jesuskinto</a>) on
-          <a href="https://codepen.io">CodePen</a>.
-        </iframe>
-      </div>
-      <div class="column is-6">
-        <iframe
-          data-aos="fade"
-          scrolling="no"
-          title="Complex background patterns (CSS SECRETS)"
-          src="https://codepen.io/jesuskinto/embed/preview/eYpBZvX?height=265&theme-id=light&default-tab=result"
-          frameborder="no"
-          allowtransparency="true"
-          allowfullscreen="true"
-          loading="lazy"
-        >
-          See the Pen
-          <a
-            href="https://codepen.io/jesuskinto/pen/eYpBZvX"
-          >Complex background patterns (CSS SECRETS)</a> by Quinto Jesús
+          <a :href="`https://codepen.io/jesuskinto/pen/${r}`">Clock</a> by Quinto Jesús
           (
           <a href="https://codepen.io/jesuskinto">@jesuskinto</a>) on
           <a href="https://codepen.io">CodePen</a>.
@@ -83,6 +27,22 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      results: [
+        'qBOXXWE',
+        'LYpYYXO',
+        'gOapmMj',
+        'wvKGrwE',
+        'eYpBZvX'
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
 .container
